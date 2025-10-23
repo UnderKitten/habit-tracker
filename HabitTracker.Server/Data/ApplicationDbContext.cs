bce9db0c4815c58019a9b03b6a5960142d9fd76d
Maxim Churzin <mchurzin@outlook.com>
2025-10-23 11:52:22 -0400

    Add models and DbContext

---
+++ HabitTracker.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HabitTracker.Server.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabitTracker.Server.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Habit> Habits { get; set; }
+        public DbSet<HabitLog> HabitLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Habit>().ToTable("Habit");
+            builder.Entity<HabitLog>().ToTable("HabitLog");
+        }
+    }
+}
---
+++ HabitTracker.Server/Models/Habit.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HabitTracker.Server.Models
+{
+    public class Habit
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+        public int Frequency { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public string ColorHex { get; set; }
+        public List<HabitLog> HabitLogs { get; set; }
+    }
+}
