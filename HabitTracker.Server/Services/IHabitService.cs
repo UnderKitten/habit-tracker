fd4420709f1bf18b2703feacf23c5df599c25dff
Maxim Churzin <mchurzin@outlook.com>
2025-10-24 11:47:58 -0400

    Create IHabitService

---
+++ HabitTracker.Server/Services/IHabitService.cs
@@ -0,0 +1,13 @@
+﻿using HabitTracker.Server.DTOs;
+
+namespace HabitTracker.Server.Services
+{
+    public interface IHabitService
+    {
+        Task<HabitResponseDto> CreateHabitAsync(string userId, CreateHabitDto dto);
+        Task<HabitResponseDto> UpdateHabitAsync(string userId, CreateHabitDto dto, Guid id);
+        Task<bool> DeleteHabitAsync(string userId, Guid id);
+        Task<HabitResponseDto?> GetHabitAsync(string userId, Guid id);
+        Task<List<HabitResponseDto>> GetAllHabitsAsync(string userId);
+    }
+}
