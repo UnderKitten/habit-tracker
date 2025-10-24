d9e30d58a4428132a393aa3cf651f388fa871a7d
Maxim Churzin <mchurzin@outlook.com>
2025-10-24 11:47:40 -0400

    Create Habit DTOs

---
+++ HabitTracker.Server/DTOs/CreateHabitDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HabitTracker.Server.DTOs
+{
+    public class CreateHabitDto
+    {
+        [MaxLength(100)]
+        public required string Name { get; set; }
+        [MaxLength(500)]
+        public string? Description { get; set; }
+        [RegularExpression("^#[0-9A-Fa-f]{6}$", ErrorMessage = "Invalid color format")]
+        public required string ColorHex { get; set; }
+        public required int Frequency { get; set; }
+    }
+}
